7304bbc731d54b13e8ea1e210b984e6d16d355b8
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-27 00:45:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -135,6 +135,14 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+
+   if(e.which===LEFT_ARROW){
+     moveDodgerLeft()
+   }
+   if(e.which===RIGHT_ARROW){
+     moveDodgerRight()
+   }
+   
 }
 
 function moveDodgerLeft() {
@@ -143,6 +151,9 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   
+   
+   
 }
 
 function moveDodgerRight() {
